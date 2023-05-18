6ccb1f743db0d578855c027e1db291bed01b523c
Antonio Luis Garcia <antonlugar777@gmail.com>
2023-05-18 17:58:52 +0200

    Creacion de ProductoService

---
+++ Proyecto/src/main/java/agg/service/ProductoService.java
@@ -0,0 +1,19 @@
+package agg.service;
+
+import agg.client.ProductoClient;
+import agg.persistence.dao.clases.Productos.Producto;
+
+import java.util.List;
+
+public class ProductoService {
+
+    private ProductoClient productoClient;
+
+    public ProductoService(ProductoClient productoClient){
+        this.productoClient = productoClient;
+    }
+
+    public List<Producto> lisAll(){
+        return productoClient.listAll();
+    }
+}
