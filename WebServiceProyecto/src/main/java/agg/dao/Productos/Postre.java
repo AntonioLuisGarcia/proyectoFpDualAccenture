756f92ae1c9dbe13bf57b7d9112dcf8b90e090c2
Antonio Luis Garcia <antonlugar777@gmail.com>
2023-05-18 21:14:50 +0200

    Creacion de constructor en Postre

--- WebServiceProyecto/src/main/java/agg/dao/Productos/Postre.java
+++ WebServiceProyecto/src/main/java/agg/dao/Productos/Postre.java
@@ -12,4 +12,10 @@ public class Postre extends Producto {
 
     private int kcal;
     private int personasParaCompartir;
+
+    public Postre(int id, double precio, String nombre, String descripcion, String imagen, int kcal, int personasParaCompartir){
+        super(id, precio, nombre, descripcion, imagen);
+        this.kcal = kcal;
+        this.personasParaCompartir = personasParaCompartir;
+    }
 }
