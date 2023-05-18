ee299da3b99b9d2f479ab2c73b32fd60a81fef92
Antonio Luis Garcia <antonlugar777@gmail.com>
2023-05-18 21:24:44 +0200

    Creacion del metodo getById en ProductoController

--- WebServiceProyecto/src/main/java/agg/controller/ProductoController.java
+++ WebServiceProyecto/src/main/java/agg/controller/ProductoController.java
@@ -4,12 +4,15 @@ import agg.dao.Productos.Producto;
 import agg.persistence.manager.BebidaManager;
 import agg.persistence.manager.ComidaManager;
 import agg.persistence.manager.PostreManager;
+import agg.persistence.manager.ProductoManager;
 import agg.persistence.service.BebidaService;
 import agg.persistence.service.ComidaService;
 import agg.persistence.service.PostreService;
+import agg.persistence.service.ProductoService;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
@@ -18,11 +21,14 @@ import java.util.List;
 
 @Path("/productos")
 public class ProductoController {
+
+    private ProductoService productoService;
     private ComidaService comidaService;
     private BebidaService bebidaService;
     private PostreService postreService;
 
     public ProductoController(){
+        this.productoService = new ProductoService(new ProductoManager());
         this.comidaService = new ComidaService(new ComidaManager());
         this.bebidaService = new BebidaService(new BebidaManager());
         this.postreService = new PostreService(new PostreManager());
@@ -37,5 +43,13 @@ public class ProductoController {
         products.addAll(bebidaService.getAllDrinks());
         products.addAll(postreService.getAllDesserts());
         return Response.ok().entity(products).build();
+    }
+
+    @GET
+    @Path("/getById")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response Producto(@QueryParam("id") int id){
+        Producto producto = productoService.getById(id);
+        return Response.ok().entity(producto).build();
     }
 }
