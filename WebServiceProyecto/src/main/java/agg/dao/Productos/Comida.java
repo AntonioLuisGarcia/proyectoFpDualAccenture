7ff7b461377ceeab98a1d85aec85d41b6ffa2c0e
Antonio Luis Garcia <antonlugar777@gmail.com>
2023-05-18 21:15:38 +0200

    Creacion de constructor en Comida

--- WebServiceProyecto/src/main/java/agg/dao/Productos/Comida.java
+++ WebServiceProyecto/src/main/java/agg/dao/Productos/Comida.java
@@ -12,4 +12,10 @@ public class Comida extends Producto {
 
     private boolean vegano;
     private int tiempoDePreparacion;
+
+    public Comida(int id, double precio, String nombre, String descripcion, String imagen, boolean vegano, int tiempoDePreparacion){
+        super(id, precio, nombre, descripcion, imagen);
+        this.vegano = vegano;
+        this.tiempoDePreparacion = tiempoDePreparacion;
+    }
 }
