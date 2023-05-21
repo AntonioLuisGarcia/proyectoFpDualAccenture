040213ebbcd28ffb416404ac15d2b7b3638c7c4d
Antonio Luis Garcia <antonlugar777@gmail.com>
2023-05-21 18:54:01 +0200

    Metodos getNoPagadas y pagarComanda en ComandaManager

--- WebServiceProyecto/src/main/java/agg/persistence/manager/ComandaManager.java
+++ WebServiceProyecto/src/main/java/agg/persistence/manager/ComandaManager.java
@@ -1,14 +1,13 @@
 package agg.persistence.manager;
 
 import agg.dao.Comanda;
-import agg.persistence.service.CamareroService;
+import agg.dao.ComandaProducto;
 import agg.persistence.service.ComandaProductoService;
-import agg.persistence.service.MesaService;
+import agg.persistence.service.ComandaService;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ComandaManager {
 
@@ -20,9 +19,10 @@ public class ComandaManager {
                         result.getInt("IdComanda")
                         , result.getInt("IdMesa")
                         , result.getInt("IdCamarero")
-                        , result.getTimestamp("FechaLlegada").toLocalDateTime()
+                        , result.getTimestamp("FechaLlegada").toString()
                         , result.getString("EmailContacto")
-                        , new ComandaProductoService(new ComandaProductoManager()).getProductosById(result.getInt("IdComanda")));
+                        , new ComandaProductoService(new ComandaProductoManager()).getProductosById(result.getInt("IdComanda"))
+                        , result.getBoolean("Pagada"));
             } else {
                 return null;
             }
@@ -31,22 +31,85 @@ public class ComandaManager {
         }
     }
 
-    public int createComanda(Connection con, int idMesa, int idCamarero, String emailContacto){
-        try (PreparedStatement stm = con.prepareStatement("INSERT INTO 'mydb'.'comanda' ('IdCamarero', 'IdMesa', 'FechaLlegada', 'EmailContacto') VALUES (" + idCamarero + "," + idMesa + ", NOW()" + emailContacto + ")")) {
+    public Comanda createComanda(Connection con, int idMesa, int idCamarero, String emailContacto){
+        try (PreparedStatement stm = con.prepareStatement("INSERT INTO `mydb`.`comanda` (`IdCamarero`, `IdMesa`, `FechaLlegada`, `EmailContacto`, `Pagada`) VALUES (?, ?, NOW(), ?, ?)", Statement.RETURN_GENERATED_KEYS)) {
+            stm.setInt(1, idCamarero);
+            stm.setInt(2, idMesa);
+            stm.setString(3, emailContacto);
+            stm.setBoolean(4, false);
 
-            int affectedRows = stm.executeUpdate();
+        int affectedRows = stm.executeUpdate();
 
             if (affectedRows > 0) {
-                ResultSet generatedKey = stm.getGeneratedKeys();///preguntar si hay que hacer try catch
+                ResultSet generatedKey = stm.getGeneratedKeys();
                 if(generatedKey.next()){
-                    return generatedKey.getInt(1);
+                   return new ComandaService(new ComandaManager()).getById(generatedKey.getInt(1));
                 }
-                return affectedRows;
+                return new ComandaService(new ComandaManager()).getById(generatedKey.getInt(1));
             } else {
-                return 0;
+                return null;
             }
         } catch (SQLException e) {
-            return 0;
+            return null;
         }
     }
+
+    public List<Comanda> listAll(Connection con){
+        try(PreparedStatement stm = con.prepareStatement("SELECT * FROM comanda")){
+            ResultSet result = stm.executeQuery();
+            List<Comanda> comandas = new ArrayList<>();
+
+            while(result.next()){
+                comandas.add(new Comanda(result.getInt("IdComanda")
+                        , result.getInt("IdCamarero")
+                        , result.getInt("IdMesa")
+                        , result.getString("FechaLlegada")
+                        , result.getString("EmailContacto")
+                        , new ComandaProductoService(new ComandaProductoManager()).getProductosById(result.getInt("IdComanda"))
+                        , result.getBoolean("Pagada")));
+            }
+
+            return comandas;
+        }catch (SQLException e){
+            return null;
+        }
+    }
+
+    public List<Comanda> getNoPagadas(Connection con){
+        try(PreparedStatement stm = con.prepareStatement("SELECT * FROM `comanda` WHERE `Pagada` = 0")){//cuidado las comillas
+            ResultSet result = stm.executeQuery();
+            List<Comanda> comandas = new ArrayList<>();
+
+            while(result.next()){
+                comandas.add(new Comanda(result.getInt("IdComanda")
+                        , result.getInt("IdCamarero")
+                        , result.getInt("IdMesa")
+                        , result.getString("FechaLlegada")
+                        , result.getString("EmailContacto")
+                        , new ComandaProductoService(new ComandaProductoManager()).getProductosById(result.getInt("IdComanda"))
+                        , result.getBoolean("Pagada")));
+            }
+
+            return comandas;
+        }catch (SQLException e){
+            return null;
+        }
+    }
+
+    public Comanda pagarComanda(Connection con, int id){
+        try (PreparedStatement stm = con.prepareStatement("UPDATE `comanda` SET `Pagada` = ? WHERE `IdComanda` = ?")) {
+            stm.setBoolean(1, true);
+            stm.setInt(2, id);
+
+            int affectedRows = stm.executeUpdate();
+            if (affectedRows > 0) {
+                return new ComandaService(new ComandaManager()).getById(id);
+            } else {
+                return null;
+            }
+        }catch (SQLException e){
+            return null;
+        }
+    }
+
 }
