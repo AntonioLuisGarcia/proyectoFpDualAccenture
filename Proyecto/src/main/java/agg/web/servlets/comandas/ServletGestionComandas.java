89c561cf15c69b1520f70ede5685aab3fd41710c
Antonio Luis Garcia <antonlugar777@gmail.com>
2023-05-22 14:09:22 +0200

    Servlet para gestionar Comandas que trae las comandas no pagadas y las muestra en gestionComandas.jsp

---
+++ Proyecto/src/main/java/agg/web/servlets/comandas/ServletGestionComandas.java
@@ -0,0 +1,43 @@
+package agg.web.servlets.comandas;
+
+import agg.client.ComandaClient;
+import agg.persistence.dao.clases.Comanda;
+import agg.service.ComandaService;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name="ServletGestionComandas", urlPatterns ={"/servlet-gestionComandas"})
+public class ServletGestionComandas extends HttpServlet {
+
+    @Override
+    public void init()throws ServletException {
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //Creamos un servicio para poder recoger todas las comandas
+        ComandaService comandaService = new ComandaService(new ComandaClient());
+
+        //Guardamos todas las comandas en una Lista
+        List<Comanda> comandas = comandaService.getNoPagadas();
+
+        //Guardamos la lista para mostrarla en gestionComandas.jsp
+        req.setAttribute("comandas",comandas);
+
+        //Redirigimos a gestionComandas.jsp
+        req.getRequestDispatcher("/gestionComandas/gestionComandas.jsp").forward(req, resp);
+
+    }
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse res)
+            throws ServletException, IOException {
+        doPost(req, res);
+    }
+}
