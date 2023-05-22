a165c8821c48fea35d2c0b4a824c9533f6f25872
Antonio Luis Garcia <antonlugar777@gmail.com>
2023-05-22 14:11:40 +0200

    Servlet para cambiar el estado de una comanda a pagada y devuelve la comanda para mostrarla en un jsp

---
+++ Proyecto/src/main/java/agg/web/servlets/comandas/ServletPagarComanda.java
@@ -0,0 +1,36 @@
+package agg.web.servlets.comandas;
+
+import agg.client.ComandaClient;
+import agg.persistence.dao.clases.Comanda;
+import agg.service.ComandaService;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name="ServletPagarComanda", urlPatterns ={"/servlet-pagarComanda"})
+public class ServletPagarComanda extends HttpServlet {
+    @Override
+    public void init()throws ServletException {
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        int id = Integer.parseInt(req.getParameter("idComanda"));
+
+        Comanda comanda = new ComandaService(new ComandaClient()).pagarComanda(id);
+
+        req.setAttribute("comanda",comanda);
+        req.getRequestDispatcher("/finalizarComanda/finalizarComanda.jsp").forward(req, resp);
+
+    }
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse res)
+            throws ServletException, IOException {
+        doPost(req, res);
+    }
+}
